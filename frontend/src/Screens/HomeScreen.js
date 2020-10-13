64ea1c174778de9cb052ab93b129a40c243ccd78
Desmond Brown <browndesmond30@yahoo.com>
2020-10-13 15:08:02 +0000

    Kali: Fixed HomeScreen for fetching of products from DB and populating view

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,57 +1,63 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect, useContext} from 'react'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { listProducts } from '../actions/productActions';
 
-import Header from '../components/Header';
-import Sidebar from '../components/Sidebar';
+import Base from '../components/Base';
+import { AppContext } from '../App';
 
 
 function HomeScreen(props){
-  //defining hooks
-const productList = useSelector(state => state.productList);
-const { products, loading, error } = productList;
-const dispatch = useDispatch();
+
+const app_context = useContext(AppContext);
 
 
 useEffect(() => {
-  dispatch( listProducts());
+  if (app_context.storeState.products.length <= 0) {
+    axios.get('/api/products/')
+    .then(response=> {
+      console.log(response.data);
+      app_context.dispatchStoreState({key: "SET_PRODUCTS", payload: response.data})
+    })
+    .catch(exception=> {
+      console.log(exception);
+    })
+  }
   return () => {
     //
   }; 
 }, [])
 
 
-return loading ? <div>Loading...</div> : error ? <div>{error}</div> :
+return app_context.appState.isLoading ? <div>Loading...</div> : app_context.appState.error ? <div>{app_context.appState.error}</div> :
 ( 
   <>
-    <Header />
-    <Sidebar />
-    
-      <main className="main">
-          <div className="content"> 
-            <ul className="products">
-              {
-                products.map(product =>
-                  <li key={product._id}>
-                    <div className="product">
-                        <Link to={'/product/' + product._id}>
-                          <img className="product-img" src={product.img} alt="product" />
-                        </Link>
-                      
-                        <div className="prouduct-name">
-                              <Link to={'/product/' + product._id}>{product.name}</Link>
-                        </div>
-                        <div className="prouduct-brand">{product.brand}</div>
-                        <div className="product-price">{product.price}</div>
-                        <div className="product-rating">{product.rating} Stars ({product.numReviews})</div>
-                    </div>
-                </li>)
-              }
-            </ul>
-          </div>
-        </main>
+    {/* <Header /> */}
+    <Base sidebar={true} header={true}>
+      {/* <Sidebar /> */}
+        <main className="main">
+            <div className="content"> 
+              <ul className="products">
+                {
+                  app_context.storeState.products.map(product =>
+                    <li key={product._id}>
+                      <div className="product">
+                          <Link to={'/product/' + product._id}>
+                            <img className="product-img" src={product.img} alt="product" />
+                          </Link>
+                        
+                          <div className="prouduct-name">
+                                <Link to={'/product/' + product._id}>{product.name}</Link>
+                          </div>
+                          <div className="prouduct-brand">{product.brand}</div>
+                          <div className="product-price">{product.price}</div>
+                          <div className="product-rating">{product.rating} Stars ({product.numReviews})</div>
+                      </div>
+                  </li>)
+                }
+              </ul>
+            </div>
+          </main>
+    </Base>
   </>
 )
       
